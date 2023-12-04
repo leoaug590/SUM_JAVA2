7b1bd2375ddc7bbadc000f69d206cd7d1fd1813e
leoaug590 <kcc6950@naver.com>
2023-12-04 11:34:31 +0900

    1챕터 실습 코드

---
+++ Chap01/src/Hello2.java
@@ -0,0 +1,17 @@
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		System.out.print("Welcom to Java"); // 줄바꿈 X
+		System.out.print("객체지향 프로그래밍 언어"); // 줄바꿈 X
+		System.out.println(); // 줄바꿈 O
+		
+		System.out.println("Welcom to Java"); // 줄바꿈 X
+		System.out.println("객체지향 프로그래밍 언어"); // 줄바꿈 X
+		
+		System.out.println(); // 줄바꿈 O
+		System.out.println("Hello!! Java - second"); // 줄바꿈 O
+	}
+
+}
